41eeffc46d346a428645cf42543b7dc9355f0e78
valbeat <kj1ktk@gmail.com>
2017-10-18 23:51:00 +0900

    [update] インスタンス生成が重複しなくなったので元に戻す

--- xunit.py
+++ xunit.py
@@ -23,12 +23,10 @@ class WasRun(TestCase):
 
 class TestCaseTest(TestCase):
 
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
-        self.test.run()
-        assert("setUp testMethod " == self.test.log)
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod " == test.log)
 
 
 TestCaseTest("testTemplateMethod").run()
