b6951c641073b1b44ab85aa45bdbc50c325826b8
valbeat <kj1ktk@gmail.com>
2017-10-18 22:43:13 +0900

    [update] メソッド名から動的出し
    このような呼び出し方をPluggable Selectorと呼ぶ

--- xunit.py
+++ xunit.py
@@ -1,13 +1,18 @@
 class WasRun:
-    def __init__(self,name):
+
+    def __init__(self, name):
         self.wasRun = None
+        self.name = name
+
     def Run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
+
     def testMethod(self):
         self.wasRun = 1
+
 
 test = WasRun("testMethod")
 print(test.wasRun)
 test.Run()
 print(test.wasRun)
-
